8eda404767fa334907062bc4c9ecf86bfca94e0d
andys <andysmith836@gmail.com>
2019-07-08 21:49:52 +0100

    Remove duplication

--- src/diamond_clojure/diamond_maker.clj
+++ src/diamond_clojure/diamond_maker.clj
@@ -3,44 +3,34 @@
 
 (use '[clojure.string :only [index-of]])
 
-(defn get-index-for-char [char]
-  (index-of "ABCDEFGHIJKLMNOPQRSTUVWXYZ" char))
+(def alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-(defn char-for-index [index]
-  (str (.charAt "ABCDEFGHIJKLMNOPQRSTUVWXYZ" index)))
+(defn get-index-for-char [char] (index-of alphabet char))
 
-(defn calc-leading-spaces [char index]
-  (- (get-index-for-char char) index))
+(defn char-for-index [index] (str (.charAt alphabet index)))
 
-(defn gen-spaces [num]
-  (apply str (take num (repeat " "))))
+(defn calc-leading-spaces [char index] (- (get-index-for-char char) index))
 
-(defn gen-leading-spaces [char index]
-  (gen-spaces (calc-leading-spaces char index)))
+(defn gen-spaces [num] (apply str (take num (repeat " "))))
 
-(defn calc-middle-spaces [char]
-  (+ 1 (* 2 (- (get-index-for-char char) 1))))
+(defn gen-leading-spaces [char index] (gen-spaces (calc-leading-spaces char index)))
 
-(defn gen-middle-spaces [char]
-  (gen-spaces (calc-middle-spaces char )))
+(defn calc-middle-spaces [char] (+ 1 (* 2 (- (get-index-for-char char) 1))))
+
+(defn gen-middle-spaces [char] (gen-spaces (calc-middle-spaces char )))
 
 (defn draw-line [char index]
-  (apply str (gen-leading-spaces char index) (char-for-index index) (gen-middle-spaces (char-for-index index)) (char-for-index index) "\n"))
+  (let [current-char (char-for-index index)]
+    (apply str (gen-leading-spaces char index) current-char (gen-middle-spaces current-char) current-char "\n")))
 
-(defn generate-tip [char]
-  (str (gen-leading-spaces char 0) "A\n"))
+(defn generate-tip [char] (str (gen-leading-spaces char 0) "A\n"))
 
-(defn draw [char r]
-  (apply str (for [x r] (draw-line char x))))
+(defn draw [char r] (apply str (for [x r] (draw-line char x))))
 
-(defn draw-top [char]
-  (draw char (range 1 (get-index-for-char char))))
+(defn draw-middle [char]
+  (draw char (concat (range 1 (get-index-for-char char)) (range (get-index-for-char char) 0 -1))))
 
-(defn draw-bottom [char]
-  (draw char (range (get-index-for-char char) 0 -1)))
-
-(defn draw-body [char]
-  (apply str (generate-tip char) (draw-top char) (draw-bottom char) (generate-tip char)))
+(defn draw-body [char] (apply str (generate-tip char) (draw-middle char) (generate-tip char)))
 
 (defn diamond-maker [char]
   (if (= char "A")
